92edadebcd2a4e0a25f92cb3f2bce6266f0988c7
Ashen Shamilka Wickramasinghe <128624342+Ashen270@users.noreply.github.com>
2025-06-13 00:51:49 +0530

    backend intitated

---
+++ server/dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+// Routes import
+// Configurations
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, helmet_1.default)());
+app.use(helmet_1.default.crossOriginEmbedderPolicy({ policy: "require-corp" }));
+app.use((0, morgan_1.default)('common'));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+// routes
+app.get('/', (req, res) => {
+    res.send('this is the home route!');
+});
+//server
+const PORT = process.env.PORT || 4001;
+;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// Routes import
+
+
+
+// Configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "require-corp" }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use (cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('this is the home route!');
+});
+
+
+//server
+
+const PORT = process.env.PORT || 4001;;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

c089d871dc296559bd08b9aefdb3d5d58755883d
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2026-06-07 02:31:19 +0530

    Allow remote images and add S3 bucket env
    Update Next.js config to allow external images from example.com and any *.amazonaws.com host via images.remotePatterns, enabling loading assets from those domains. Also add S3_BUCKET_NAME to server/.env to configure the application S3 bucket for uploads/storage.

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
